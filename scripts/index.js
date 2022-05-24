31cb98ade3075bf94b1e5f55b19c79152d31ef95
Dmitriy Ryabov <dima.poltergeist.2015.@yandex.ru>
2022-05-24 10:28:20 +0300

    popravki

--- scripts/index.js
+++ scripts/index.js
@@ -7,32 +7,32 @@ const nameInput = popup.querySelector('.popup__input_text_heading')
 
 const profileSubtitleElement = document.querySelector('.profile__subtitle')
 const nameSubheadingElement = document.querySelector('.popup__input_text_subheading')
-const POPUP_IS_OPEN_CLASSNAME = 'popup_opened';
+const popupOpen = 'popup_opened';
 
 const formPopupElement = document.querySelector('.popup__form')
 
 
-function openPopup(profileEditBtn) {//если я удаляю аргумент, код перестает работать((
-    profileEditBtn.classList.add(POPUP_IS_OPEN_CLASSNAME)
+function openPopup() {
+    popup.classList.add(popupOpen)
 }
 
-function closePopup(popupCloseBtn) {
-    popupCloseBtn.classList.remove(POPUP_IS_OPEN_CLASSNAME)
+function closePopup() {
+    popup.classList.remove(popupOpen)
 }
 
 profileEditBtn.addEventListener('click', function () {
-    openPopup(popup);
+    openPopup();
     nameInput.value = profileTitleElement.textContent;
     nameSubheadingElement.value = profileSubtitleElement.textContent;
 })
 
 popupCloseBtn.addEventListener('click', function () {
-    closePopup(popup)
+    closePopup()
 })
 
 formPopupElement.addEventListener('submit', function (event) {
     event.preventDefault()
     profileTitleElement.textContent = nameInput.value;
     profileSubtitleElement.textContent = nameSubheadingElement.value;
-    closePopup(popup)
+    closePopup()
 })
