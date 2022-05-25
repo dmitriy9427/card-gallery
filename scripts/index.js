630d7667aa8aa52dbdf78d044099362279361b36
Dmitriy Ryabov <dima.poltergeist.2015.@yandex.ru>
2022-05-25 08:44:08 +0300

    peerhod v vetku

--- scripts/index.js
+++ scripts/index.js
@@ -11,6 +11,7 @@ const nameTextInput = document.querySelector('.popup__input_text_subheading')
 const formPopupElement = document.querySelector('.popup__form')
 
 
+//попап редактирования профиля
 function openPopup() {
     nameInput.value = profileTitleElement.textContent;
     nameTextInput.value = profileSubtitleElement.textContent;
@@ -33,3 +34,25 @@ formPopupElement.addEventListener('submit', function (event) {
     profileSubtitleElement.textContent = nameTextInput.value;
     closePopup()
 })
+
+
+const profileAddBtn = document.querySelector('.profile__add-btn');
+const popuup = document.querySelector('.popuup');
+const closePopuupBtn = document.querySelector('.popuup__close');
+const popuupFormInput = document.querySelector('.popuup__form');
+const imgRemoveBtn = document.querySelector('.elements__delete-btn')
+
+//открытие попапа добавления картинок!!!
+function openPopuup() {
+    popuup.classList.add('popuup_opened');
+}
+
+function closePopuup() {
+    popuup.classList.remove('popuup_opened');
+}
+
+profileAddBtn.addEventListener('click', openPopuup);
+
+closePopuupBtn.addEventListener('click', closePopuup);
+
+
